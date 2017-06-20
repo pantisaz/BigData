21e54c02d4206f00915517c411d02013c3276ab8
Santiago Paz <santiago.l.paz@gmail.com>
2017-06-20 22:59:39 +0200

    first commit

---
+++ R/mychol.R
@@ -0,0 +1,12 @@
+my.chol <- function(A) {
+  l <- matrix(0, nrow(A), nrow(A))
+  l[1, 1] <- sqrt(A[1, 1])
+  # Calculate elements
+  for (i in 2:nrow(A)) {
+    for (j in 1:i-1) {
+      l[i, j] <- 1/l[j, j] * (A[i, j] - sum(l[i, 1:j-1] * l[j, 1:j-1]))
+      l[i, i] <- sqrt(A[i, i] - sum(l[i, 1:i-1]**2))
+    }
+  }
+  return(l)
+}
